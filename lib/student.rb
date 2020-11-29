d33ba03dadcac3c85a8d2f5e9b40f598929246b8
racross1 <racross1@gmail.com>
2020-11-29 15:34:19 -0800

    inheritance 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require './user.rb'
+
+class Student < User
+
+    attr_accessor :knowledge, :first_name, :last_name
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge = knowledge
+    end
+
 
 end
